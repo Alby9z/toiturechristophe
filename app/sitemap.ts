d3f05ef096a2e91451c86eccae1b5478b0399dd4
Xaluks <lukas.dias2004@gmail.com>
2026-08-22 15:45:19 +0200

    correction

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://lestoituresdechristophe.ccom";
+  const baseUrl = "https://lestoituresdechristophe.com";
 
   return [
     {
